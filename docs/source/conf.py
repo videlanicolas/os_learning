cf71438a3da6d1ee0c94d7e44ffde6ea99688615
Nicolas Videla <videlanicolas@gmail.com>
2024-04-30 19:18:43 +1000

    still with problems....

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ extensions = ["myst_parser"]
 
 templates_path = ['_templates']
 exclude_patterns = []
-root_doc = 'source/index'
+# root_doc = 'source/index'
 
 
 # -- Options for HTML output -------------------------------------------------
